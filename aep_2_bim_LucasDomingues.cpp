5b447bd8be2e8af0e606323c6ce73920b522c7b7
Lucas Domingues <149822932+aleatorio101@users.noreply.github.com>
2024-11-02 23:34:21 -0300

    Mudanças na criptografia e tamanho de usuario, senha e email

--- aep_2_bim_LucasDomingues.cpp
+++ aep_2_bim_LucasDomingues.cpp
@@ -3,12 +3,15 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
+#include <time.h>
 
 #define MAX_USERS 100
-#define MAX_NAME 50
-#define MAX_EMAIL 50
-#define MAX_PASSWORD 50
+#define MAX_NAME 100  
+#define MAX_EMAIL 100 
+#define MAX_PASSWORD 100 
 #define FILENAME "users.txt"
+#define ENCRYPTION_KEY "S3cr3tK3y2024"
+#define SALT_LENGTH 8
 
 typedef struct {
     int id;
@@ -18,15 +21,82 @@ typedef struct {
     int active;
 } User;
 
-void encrypt(char *text) {
-    for(int i = 0; text[i] != '\0'; i++) {
-        text[i] = text[i] + 3;
+
+void generateSalt(char *salt) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (int i = 0; i < SALT_LENGTH; i++) {
+        int index = rand() % (sizeof(charset) - 1);
+        salt[i] = charset[index];
+    }
+    salt[SALT_LENGTH] = '\0';
+}
+
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
     }
 }
 
+
+void encrypt(char *text) {
+    char salt[SALT_LENGTH + 1];
+    generateSalt(salt);
+    
+    int textLen = strlen(text);
+    int keyLen = strlen(ENCRYPTION_KEY);
+    int saltLen = strlen(salt);
+    
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        text[i] = text[i] ^ ENCRYPTION_KEY[i % keyLen];
+    }
+    
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        int shift = (salt[i % saltLen] + i) % 26;
+        text[i] = ((text[i] + shift) % 128);
+    }
+    
+    
+    reverseString(text);
+    
+    
+    char temp[256];
+    strcpy(temp, text);
+    strcpy(text, salt);
+    strcat(text, temp);
+}
+
+
 void decrypt(char *text) {
-    for(int i = 0; text[i] != '\0'; i++) {
-        text[i] = text[i] - 3;
+    if (strlen(text) <= SALT_LENGTH) {
+        return;
+    }
+
+    char salt[SALT_LENGTH + 1];
+    
+    strncpy(salt, text, SALT_LENGTH);
+    salt[SALT_LENGTH] = '\0';
+    
+    memmove(text, text + SALT_LENGTH, strlen(text) - SALT_LENGTH + 1);
+    
+
+    reverseString(text);
+    
+    
+    for (int i = 0; text[i] != '\0'; i++) {
+        int shift = (salt[i % SALT_LENGTH] + i) % 26;
+        text[i] = ((text[i] - shift + 128) % 128);
+    }
+    
+    
+    int keyLen = strlen(ENCRYPTION_KEY);
+    for (int i = 0; text[i] != '\0'; i++) {
+        text[i] = text[i] ^ ENCRYPTION_KEY[i % keyLen];
     }
 }
 
@@ -99,10 +169,15 @@ int isValidEmail(const char *email) {
     return strchr(email, '@') != NULL;
 }
 
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void addUser(User users[], int *count) {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     if (*count >= MAX_USERS) {
-        printf("Limite de usu�rios atingido!\n");
+        printf("Limite de usuários atingido!\n");
         return;
     }
 
@@ -110,30 +185,33 @@ void addUser(User users[], int *count) {
     newUser.id = *count + 1;
     newUser.active = 1;
 
-    printf("Nome: ");
-    scanf(" %[^\n]", newUser.name);
+    printf("\nNome: ");
+    clearInputBuffer();
+    fgets(newUser.name, MAX_NAME, stdin);
+    newUser.name[strcspn(newUser.name, "\n")] = 0;
     
     do {
-        printf("Email: ");
-        scanf(" %[^\n]", newUser.email);
+        printf("\nEmail: ");
+        fgets(newUser.email, MAX_EMAIL, stdin);
+        newUser.email[strcspn(newUser.email, "\n")] = 0;
         if (!isValidEmail(newUser.email)) {
-            printf("Email inv�lido! Por favor, insira um email v�lido.\n");
+            printf("Email inválido! Por favor, insira um email válido.\n");
         }
     } while (!isValidEmail(newUser.email));
 
-    printf("Senha: ");
-    scanf(" %[^\n]", newUser.password);
+    printf("\nSenha: ");
+    fgets(newUser.password, MAX_PASSWORD, stdin);
+    newUser.password[strcspn(newUser.password, "\n")] = 0;
 
-    encrypt(newUser.password);
     users[*count] = newUser;
     (*count)++;
     
     saveUsers(users, *count);
-    printf("Usu�rio adicionado com sucesso!\n");
+    printf("Usuário adicionado com sucesso!\n");
 }
 
 void listUsers(User users[], int count) {
-    printf("\n=== Lista de Usu�rios ===\n");
+    printf("\n=== Lista de Usuários ===\n");
     for (int i = 0; i < count; i++) {
         if (users[i].active) {
             printf("ID: %d\n", users[i].id);
@@ -147,66 +225,76 @@ void listUsers(User users[], int count) {
 void updateUser(User users[], int count) {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     int id;
-    printf("Digite o ID do usu�rio para atualizar: ");
+    printf("\nDigite o ID do usuário para atualizar: ");
     scanf("%d", &id);
+    clearInputBuffer();
 
     for (int i = 0; i < count; i++) {
         if (users[i].id == id && users[i].active) {
-            printf("Novo nome (%s): ", users[i].name);
-            scanf(" %[^\n]", users[i].name);
+            printf("\nNovo nome (%s): ", users[i].name);
+            fgets(users[i].name, MAX_NAME, stdin);
+            users[i].name[strcspn(users[i].name, "\n")] = 0;
             
-            printf("Novo email (%s): ", users[i].email);
-            scanf(" %[^\n]", users[i].email);
+            do {
+                printf("\nNovo email (%s): ", users[i].email);
+                fgets(users[i].email, MAX_EMAIL, stdin);
+                users[i].email[strcspn(users[i].email, "\n")] = 0;
+                if (!isValidEmail(users[i].email)) {
+                    printf("Email inválido! Por favor, insira um email válido.\n");
+                }
+            } while (!isValidEmail(users[i].email));
             
-            printf("Nova senha (deixe em branco para manter a atual): ");
+            printf("\nNova senha (pressione Enter para manter a atual): ");
             char newPassword[MAX_PASSWORD];
-            scanf(" %[^\n]", newPassword);
+            fgets(newPassword, MAX_PASSWORD, stdin);
+            newPassword[strcspn(newPassword, "\n")] = 0;
 
             if (strlen(newPassword) > 0) {
-                encrypt(newPassword);
                 strcpy(users[i].password, newPassword);
             }
             
             saveUsers(users, count);
-            printf("Usu�rio atualizado com sucesso!\n");
+            printf("Usuário atualizado com sucesso!\n");
             return;
         }
     }
-    printf("Usu�rio n�o encontrado!\n");
+    printf("Usuário não encontrado!\n");
 }
 
 void deleteUser(User users[], int count) {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     int id;
-    printf("Digite o ID do usu�rio para excluir: ");
+    printf("Digite o ID do usuário para excluir: ");
     scanf("%d", &id);
+    clearInputBuffer();
 
     for (int i = 0; i < count; i++) {
         if (users[i].id == id && users[i].active) {
             users[i].active = 0;
             saveUsers(users, count);
-            printf("Usu�rio exclu�do com sucesso!\n");
+            printf("Usuário excluído com sucesso!\n");
             return;
         }
     }
-    printf("Usu�rio n�o encontrado!\n");
+    printf("Usuário não encontrado!\n");
 }
 
 int main() {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+    
     User users[MAX_USERS];
     int userCount = loadUsers(users);
     int option;
-    
 
     do {
-        printf("\n=== Sistema de Gerenciamento de Usu�rios ===\n");
-        printf("1. Adicionar Usu�rio\n");
-        printf("2. Listar Usu�rios\n");
-        printf("3. Atualizar Usu�rio\n");
-        printf("4. Excluir Usu�rio\n");
+        printf("\n=== Sistema de Gerenciamento de Usuários ===\n");
+        printf("1. Adicionar Usuário\n");
+        printf("2. Listar Usuários\n");
+        printf("3. Atualizar Usuário\n");
+        printf("4. Excluir Usuário\n");
         printf("0. Sair\n");
-        printf("Escolha uma op��o: ");
+        printf("\nEscolha uma opção: ");
         scanf("%d", &option);
 
         switch(option) {
@@ -226,11 +314,9 @@ int main() {
                 printf("Saindo...\n");
                 break;
             default:
-                printf("Op��o inv�lida!\n");
+                printf("Opção inválida!\n");
         }
     } while(option != 0);
 
     return 0;
 }
-
-
